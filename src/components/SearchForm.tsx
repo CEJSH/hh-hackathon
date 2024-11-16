a47a737383df50ce4e0f0eae2ecf8beb1d904769
CEJSH <eun2763@naver.com>
2024-11-16 12:08:57 +0900

    fix error

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,7 +1,5 @@
-"use client";
-import { useSearchParams } from "next/navigation";
 import FullPageLoader from "./FullpageLoader";
-import { Dispatch, SetStateAction, Suspense } from "react";
+import { Dispatch, SetStateAction } from "react";
 // import { getSearchPageLink } from "@/src/shared/utils/link/page";
 
 export const SearchForm = ({
@@ -11,9 +9,6 @@ export const SearchForm = ({
   readyToPoll: boolean;
   setReadyToPoll: Dispatch<SetStateAction<boolean>>;
 }) => {
-  const searchParams = useSearchParams();
-  const initialSearchValue = searchParams.get("q") ?? "";
-
   //   const router = useRouter();
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -33,25 +28,23 @@ export const SearchForm = ({
   }
 
   return (
-    <Suspense fallback={<FullPageLoader message={""} />}>
-      <form
-        className="flex w-full max-w-[692px] h-[40px] sm:px-2 md:px-1"
-        onSubmit={handleSubmit}
+    <form
+      className="flex w-full max-w-[692px] h-[40px] sm:px-2 md:px-1"
+      onSubmit={handleSubmit}
+    >
+      <input
+        defaultValue={""}
+        name="query"
+        type="text"
+        className="w-full text-gray-900 outline-none px-3 py-2 h-[40px] bg-gray-50 border border-gray-300 rounded-l-[20px] text-base font-normal focus:border-blue-300 placeholder:text-gray-400"
+        placeholder="검색"
+      />
+      <button
+        type="submit"
+        className="w-[64px] h-[40px] bg-gray-200 border border-gray-300 border-l-0 rounded-r-[20px] cursor-pointer"
       >
-        <input
-          defaultValue={initialSearchValue}
-          name="query"
-          type="text"
-          className="w-full text-gray-900 outline-none px-3 py-2 h-[40px] bg-gray-50 border border-gray-300 rounded-l-[20px] text-base font-normal focus:border-blue-300 placeholder:text-gray-400"
-          placeholder="검색"
-        />
-        <button
-          type="submit"
-          className="w-[64px] h-[40px] bg-gray-200 border border-gray-300 border-l-0 rounded-r-[20px] cursor-pointer"
-        >
-          🔍
-        </button>
-      </form>
-    </Suspense>
+        🔍
+      </button>
+    </form>
   );
 };
