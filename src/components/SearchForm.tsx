6373fc143c77f2308181623641f9f61063c49812
CEJSH <eun2763@naver.com>
2024-11-16 12:05:11 +0900

    fix error

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,6 +1,6 @@
 import { useSearchParams } from "next/navigation";
 import FullPageLoader from "./FullpageLoader";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, Suspense } from "react";
 // import { getSearchPageLink } from "@/src/shared/utils/link/page";
 
 export const SearchForm = ({
@@ -32,23 +32,25 @@ export const SearchForm = ({
   }
 
   return (
-    <form
-      className="flex w-full max-w-[692px] h-[40px] sm:px-2 md:px-1"
-      onSubmit={handleSubmit}
-    >
-      <input
-        defaultValue={initialSearchValue}
-        name="query"
-        type="text"
-        className="w-full text-gray-900 outline-none px-3 py-2 h-[40px] bg-gray-50 border border-gray-300 rounded-l-[20px] text-base font-normal focus:border-blue-300 placeholder:text-gray-400"
-        placeholder="검색"
-      />
-      <button
-        type="submit"
-        className="w-[64px] h-[40px] bg-gray-200 border border-gray-300 border-l-0 rounded-r-[20px] cursor-pointer"
+    <Suspense fallback={<FullPageLoader message={""} />}>
+      <form
+        className="flex w-full max-w-[692px] h-[40px] sm:px-2 md:px-1"
+        onSubmit={handleSubmit}
       >
-        🔍
-      </button>
-    </form>
+        <input
+          defaultValue={initialSearchValue}
+          name="query"
+          type="text"
+          className="w-full text-gray-900 outline-none px-3 py-2 h-[40px] bg-gray-50 border border-gray-300 rounded-l-[20px] text-base font-normal focus:border-blue-300 placeholder:text-gray-400"
+          placeholder="검색"
+        />
+        <button
+          type="submit"
+          className="w-[64px] h-[40px] bg-gray-200 border border-gray-300 border-l-0 rounded-r-[20px] cursor-pointer"
+        >
+          🔍
+        </button>
+      </form>
+    </Suspense>
   );
 };
